359b56736b5f1a914a4bc369b27e9fdeaf336460
sbx_user1051 <sbx_user1051@169.254.159.146>
2024-12-20 16:49:03 +0000

    Fri Dec 20 16:49:03 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,7 +8,8 @@ def checkout(skus):
         'B': 30,
         'C': 20,
         'D': 15,
-        'E': 40
+        'E': 40,
+        'F': 10
         }
         
         item_special_offers = {
@@ -70,26 +71,31 @@ def calculate_checkout(item_counts,item_base_prices):
 
                
 class TestChk():
-    def test_checkout_solution_5A(self):
-        assert checkout_solution('AAAAA') == 200
-    def test_checkout_solution_3A(self):
-         assert checkout_solution('AAA') == 130
-    def test_checkout_solution_4A(self):
-         assert checkout_solution('AAAA') == 180
-    def test_checkout_solution_2B(self):
-         assert checkout_solution('BB') == 45
-    def test_checkout_solution_2E_0B(self):
-         assert checkout_solution('EE') == 80
-    def test_checkout_solution_2E_1B(self):
-         assert checkout_solution('EEB') == 80
-    def test_checkout_solution_2B_2E(self):
-         assert checkout_solution('BBEE') == 110
-    def test_checkout_solution_2E_2B(self):
-         assert checkout_solution('EEBB') == 110
-    def test_checkout_solution_BEBEEE(self):
-            assert checkout_solution('BEBEEE') == 160
-    def test_checkout_solution_ABCDECBAABCABBAAAEEAA(self):
-            assert checkout_solution('ABCDECBAABCABBAAAEEAA') == 665
+    def test_checkout_5A(self):
+        assert checkout('AAAAA') == 200
+    def test_checkout_3A(self):
+         assert checkout('AAA') == 130
+    def test_checkout_4A(self):
+         assert checkout('AAAA') == 180
+    def test_checkout_2B(self):
+         assert checkout('BB') == 45
+    def test_checkout_2E_0B(self):
+         assert checkout('EE') == 80
+    def test_checkout_2E_1B(self):
+         assert checkout('EEB') == 80
+    def test_checkout_2B_2E(self):
+         assert checkout('BBEE') == 110
+    def test_checkout_2E_2B(self):
+         assert checkout('EEBB') == 110
+    def test_checkout_BEBEEE(self):
+            assert checkout('BEBEEE') == 160
+    def test_checkout_ABCDECBAABCABBAAAEEAA(self):
+            assert checkout('ABCDECBAABCABBAAAEEAA') == 665
+    def test_checkout_2F(self):
+         assert checkout('FF') == 20
+    def test_checkout_3F(self):
+         assert checkout('FFF') == 20
          
+
 
 
